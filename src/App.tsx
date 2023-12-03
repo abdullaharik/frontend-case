39c31c1bde98e4c2996be73296c4591f3aeccf70
abdullaharik <mail@abdullaharik.com>
2023-12-03 22:34:29 +0300

    feat: use `CategoryList`

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
+import { CategoryList } from "./components/templates";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { URL } from "./domain/constants";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App"></div>
+      <div className="App">
+        <CategoryList url={URL.categories} />
+      </div>
     </QueryClientProvider>
   );
 }
