a29c52ec21419f93a0d16457fda055e02bd922ef
abdullaharik <mail@abdullaharik.com>
2023-12-03 22:28:59 +0300

    feat: use new props

--- src/components/organisms/MultipleSelectList/MultipleSelectList.tsx
+++ src/components/organisms/MultipleSelectList/MultipleSelectList.tsx
@@ -1,20 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { Item, MultipleSelectListProps } from "./MultipleSelectList.types";
 
 import { Input, Button, Label } from "../../atoms";
-import { CheckboxList, CheckboxListItemType } from "../../molecules";
-
-const makeUnique = (list: string[]) => [...new Set(list)];
+import {
+  CheckboxList,
+  CheckboxListItemType,
+  makeCheckboxListItem,
+} from "../../molecules";
 
 export const MultipleSelectList: React.FC<MultipleSelectListProps> = (
   props
 ) => {
-  const { list } = props;
+  const { list, buttonOnClick, labels } = props;
 
   const [searchValue, setSearchValue] = useState<string>("");
-  const [selectableList, setSelectableList] = React.useState<
-    CheckboxListItemType[]
-  >(() => makeUnique(list).map((item) => ({ value: item, checked: false })));
+  const [selectableList, setSelectableList] = useState<CheckboxListItemType[]>(
+    []
+  );
+
+  useEffect(() => {
+    setSelectableList(list.map(makeCheckboxListItem));
+  }, [list]);
 
   const filteredList = selectableList.filter(({ value }) =>
     value.toLocaleLowerCase().includes(searchValue)
@@ -31,20 +37,26 @@ export const MultipleSelectList: React.FC<MultipleSelectListProps> = (
     );
   };
 
-  const handleClickButton = () => {
+  const handleButtonOnClick = () => {
     const selectedList = selectableList
       .filter(({ checked }) => checked)
       .map(({ value }) => value);
-    console.log("Selected Items", selectedList);
+
+    buttonOnClick(selectedList);
   };
 
   return (
     <div>
       <Label>
-        Ara: <Input value={searchValue} setValue={setSearchValue} />
+        {labels.title}
+        <Input
+          value={searchValue}
+          setValue={setSearchValue}
+          placeholder={labels.placeholder}
+        />
       </Label>
       <CheckboxList list={filteredList} onClickItem={handleSelect} />
-      <Button onClick={handleClickButton}>Ara</Button>
+      <Button onClick={handleButtonOnClick}>{labels.button}</Button>
     </div>
   );
 };
