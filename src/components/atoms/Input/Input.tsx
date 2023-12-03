256e870d5cc7c98cf2868f7623adce19de450bea
abdullaharik <mail@abdullaharik.com>
2023-12-03 22:23:08 +0300

    feat: add placeholder and all input arguments

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -5,11 +5,13 @@ import type { InputProps } from "./Input.types";
 import "./Input.scss";
 
 export const Input: React.FC<InputProps> = (props) => {
-  const { value, setValue } = props;
+  const { value, setValue, ...InputArgs } = props;
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
-  return <input type="text" value={value} onChange={handleOnChange} />;
+  return (
+    <input type="text" value={value} onChange={handleOnChange} {...InputArgs} />
+  );
 };
--- src/components/atoms/Input/Input.types.ts
+++ src/components/atoms/Input/Input.types.ts
@@ -1,4 +1,4 @@
 export type InputProps = {
   value: string;
   setValue: (value: string) => void;
-};
+} & React.InputHTMLAttributes<HTMLInputElement>;
